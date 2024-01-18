5ee4003228c28ab4dfd49c15fab03cfe97b91af5
Alester-Davis <alesterkvp@gmail.com>
2024-01-18 20:25:32 +0530

    create header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,11 @@ import Signup from './pages/Signup.jsx'
 import Project from './pages/Project.jsx'
 import Dashboard from './pages/Dashboard.jsx'
 import About from './pages/About.jsx'
+import Header from './components/Header.jsx'
 export default function App() {
   return (
     <BrowserRouter>
+    <Header></Header>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/sign-in' element={<Signin/>}/>
